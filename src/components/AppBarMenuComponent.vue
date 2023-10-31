7b3b8e5d042f80afbcc67e4269ab98cb56c2b175
dev2 <ko5642027@naver.com>
2023-10-31 10:19:57 +0900

    메인 헤더에 프로필 이미지 나타나도록 처리

--- src/components/AppBarMenuComponent.vue
+++ src/components/AppBarMenuComponent.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import {useConfig, useAuth, useUser} from '@/store';
-import {computed, onMounted, ref} from 'vue';
+import { useConfig, useAuth, useUser } from '@/store';
+import { computed, onMounted, ref } from 'vue';
 
 import type UserInterface from '@/interfaces/UserInterface.ts';
 
-/** Config Store */
+import defaultImage from '@/assets/images/empty_avatar.png';
+
+/** Store */
 const configStore = useConfig();
+const authStore = useAuth();
+const userStore = useUser();
 
 /** Theme Icon Change */
 const themeIcon = computed(() =>
@@ -14,48 +18,69 @@ const themeIcon = computed(() =>
 
 const isAuth = ref(false);
 const user = ref<UserInterface>({});
+const dropdown = ref(false);
+
+/** Toggle Downdown */
+const toggleDropdown = () => {
+  dropdown.value = !dropdown.value;
+};
 
 const login = () => {
   window.location.href = '/login';
 };
 
-onMounted(async () => {
-  const authStore = useAuth();
-  const userStore = useUser();
+const goToMyInfo = () => {};
+const logout = () => {};
 
+onMounted(async () => {
   isAuth.value = authStore.isAuth;
 
   await userStore.setUserInfo();
 
   user.value = userStore.user;
-  if (user.value) {
-    console.log(user.value.email);
-  }
 });
 </script>
 
 <template>
-  <v-btn v-if="isAuth" id="menu-activator" icon @click="login">
+  <v-btn v-if="isAuth" id="menu-activator" icon @click="toggleDropdown">
     <div class="rounded-image">
       <img
-        v-if="user && user.profileImage"
-        :src="user.profileImage"
-        alt="Profile Image"
-        class="image"
-      />
-      <img
-        v-else
-        src="@/assets/images/empty_avatar.png"
+        :src="user?.profileImage || defaultImage"
         alt="Profile Image"
         class="image"
       />
     </div>
   </v-btn>
   <v-btn v-else icon @click="login">
-    <i class="mdi mdi-login" style="font-size: 30px"/>
+    <i class="mdi mdi-login" style="font-size: 30px" />
   </v-btn>
   <!-- Toggle Dark mode -->
-  <v-btn :icon="themeIcon" variant="flat" @click="configStore.toggleTheme"/>
+  <v-btn :icon="themeIcon" variant="flat" @click="configStore.toggleTheme" />
+
+  <!-- Drop Down menu -->
+  <v-menu activator="#menu-activator">
+    <v-card>
+      <v-card-text>
+        <div class="mx-auto text-center">
+          <div class="rounded-image">
+            <img
+              :src="user?.profileImage || defaultImage"
+              alt="Profile Image"
+              class="image"
+            />
+          </div>
+          <h3 v-if="user?.name">{{ user.name }}</h3>
+          <p v-if="user?.email" class="text-caption mt-1">
+            {{ user.email }}
+          </p>
+          <v-divider class="my-3" />
+          <v-btn rounded variant="text" @click="goToMyInfo">My Info</v-btn>
+          <v-divider class="my-3" />
+          <v-btn rounded variant="text" @click="logout">Logout</v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-menu>
 </template>
 
 <style scoped>
